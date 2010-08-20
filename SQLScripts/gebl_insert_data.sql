7156704f0f06c92482e1ec1813fc8d53a452c5f0
Rudolf <oracle@.(none)>
2010-08-20 20:50:39 +0200

    Testdaten erweitert

--- SQLScripts/gebl_insert_data.sql
+++ SQLScripts/gebl_insert_data.sql
@@ -8,6 +8,60 @@ INSERT INTO geodaten
 VALUES(1001,to_number('48,15748','99D99999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('15,89213','99D99999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Testbrutstaette 1');
 INSERT INTO geodaten 
 VALUES(1010,to_number('48,2500','99D99999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('15,3333','99D99999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Testbrutstaette 2');
+INSERT INTO geodaten 
+VALUES(1013,to_number('48,175740','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,258629','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Hietzing');
+INSERT INTO geodaten 
+VALUES(1014,to_number('48,163756','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,445798','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Simmering');
+INSERT INTO geodaten 
+VALUES(1015,to_number('48,277035','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,404805','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Floridsdorf');
+INSERT INTO geodaten 
+VALUES(1016,to_number('47,602463','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,292874','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Wiesmath ');
+INSERT INTO geodaten 
+VALUES(1017,to_number('48,017878','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,263725','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Pfaffstaetten');
+INSERT INTO geodaten 
+VALUES(1018,to_number('48,006131','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,232377','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Baden bei Wien');
+INSERT INTO geodaten 
+VALUES(1019,to_number('48,006495','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,270688','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),1,'Tribuswinkel');
+INSERT INTO geodaten 
+VALUES(1020,to_number('47,975774','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,322092','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Oberwaltersdorf');
+INSERT INTO geodaten 
+VALUES(1021,to_number('48,122036','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,337999','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Voesendorf');
+INSERT INTO geodaten 
+VALUES(1022,to_number('47,503130','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,504608','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Oberpullendorf');
+INSERT INTO geodaten 
+VALUES(1023,to_number('47,845876','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,518073','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Eisenstadt');
+INSERT INTO geodaten 
+VALUES(1024,to_number('48,232138','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,427810','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Alte Donau');
+INSERT INTO geodaten 
+VALUES(1025,to_number('48,216562','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,396530','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Prater');
+INSERT INTO geodaten 
+VALUES(1026,to_number('48,240414','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,409211','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Donauturm');
+INSERT INTO geodaten 
+VALUES(1027,to_number('48,235182','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,481064','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Wien 22');
+INSERT INTO geodaten 
+VALUES(1028,to_number('48,147039','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,703878','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Ort an der Donau');
+INSERT INTO geodaten 
+VALUES(1029,to_number('48,113646','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,868001','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),2,'Falle Petronell');
+INSERT INTO geodaten 
+VALUES(1030,to_number('48,140883','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,477157','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Schwechat');
+INSERT INTO geodaten 
+VALUES(1031,to_number('48,192966','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,439464','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Praterauen');
+INSERT INTO geodaten 
+VALUES(1032,to_number('48,213986','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,429664','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Neue Donau');
+INSERT INTO geodaten 
+VALUES(1033,to_number('48,209206','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,372777','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Schlachthausgasse');
+INSERT INTO geodaten 
+VALUES(1034,to_number('48,170925','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,282817','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Hietzing 2');
+INSERT INTO geodaten 
+VALUES(1035,to_number('48,188784','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,274918','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Hietzing 3');
+INSERT INTO geodaten 
+VALUES(1036,to_number('48,174994','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,39417','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Siemens Wien 11');
+INSERT INTO geodaten 
+VALUES(1037,to_number('48,270278','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,421805','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Siemens Wien 21');
+INSERT INTO geodaten 
+VALUES(1038,to_number('48,422371','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,738682','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Gaenserndorf');
+INSERT INTO geodaten 
+VALUES(1039,to_number('48,233333','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),to_number('16,550833','99D999999','NLS_NUMERIC_CHARACTERS ='', '''),3,'Brutstaette Marchfeld');
 commit;
 INSERT INTO personen 
 VALUES(1002,'P-1','Rudolf','Thaler','1020','Wien','Castellezgasse 16','012165406','rudolf-thaler@tele2.at','rudi','xxxxx',1,999,'Das bin ich');
@@ -18,9 +72,49 @@ INSERT INTO brutstaetten
 VALUES(1004,'B-1',5,'1','1',1002,1001,'1','Zusatztext zu Brutstaette 1',1,'Kontaktdaten des Melders');
 INSERT INTO brutstaetten 
 VALUES(1011,'B-2',25,'1','1',1002,1010,'1','Zusatztext zu Brutstaette 2',0,NULL);
+INSERT INTO brutstaetten 
+VALUES(1040,'B-3',47,'Tuempel','leicht',1002,1030,'Handspritze','Zusatztext zu dieser Brutstaette',0,NULL);
+INSERT INTO brutstaetten 
+VALUES(1041,'B-4',47,'Tuempel','leicht',1002,1031,'Handspritze','Zusatztext zu dieser Brutstaette',0,NULL);
+INSERT INTO brutstaetten 
+VALUES(1042,'B-5',47,'Tuempel','leicht',1002,1032,'weiss nicht','Zusatztext zu dieser Brutstaette',0,'Kontaktdaten des Erfassers');
+INSERT INTO brutstaetten 
+VALUES(1043,'B-6',47,'Tuempel','leicht',1002,1033,'Handspritze','Zusatztext zu dieser Brutstaette',0,NULL);
+INSERT INTO brutstaetten 
+VALUES(1044,'B-7',47,'Tuempel','leicht',1002,1034,'Handspritze','Zusatztext zu dieser Brutstaette',1,'noch nicht vorhanden');
+INSERT INTO brutstaetten 
+VALUES(1045,'B-8',47,'Tuempel','leicht',1002,1035,'Handspritze','Zusatztext zu dieser Brutstaette',0,NULL);
+INSERT INTO brutstaetten 
+VALUES(1046,'B-9',47,'Tuempel','leicht',1002,1036,'Handspritze','Zusatztext zu dieser Brutstaette',1,'der aufmerksame Spaziergaenger');
+INSERT INTO brutstaetten 
+VALUES(1047,'B-10',47,'Tuempel','leicht',1002,1037,'Handspritze','Zusatztext zu dieser Brutstaette',0,NULL);
+INSERT INTO brutstaetten 
+VALUES(1048,'B-11',47,'Tuempel','leicht',1002,1038,'Handspritze','Zusatztext zu dieser Brutstaette',1,NULL);
+INSERT INTO brutstaetten
+VALUES(1049,'B-12',47,'Tuempel','leicht',1002,1039,'Handspritze','Zusatztext zu dieser Brutstaette',1,NULL);
 commit;
 INSERT INTO fallen 
 VALUES(1005,'F-1',1,1003,1000,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1050,'F-2',1,1003,1020,'Zusatztext zu Falle F-2Zusatztext zu Falle F-2');
+INSERT INTO fallen 
+VALUES(1051,'F-3',1,1003,1021,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1052,'F-4',1,1003,1022,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1053,'F-5',1,1003,1023,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1054,'F-6',1,1003,1024,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1055,'F-7',1,1003,1025,'Zusatztext zu Falle F-7');
+INSERT INTO fallen 
+VALUES(1056,'F-8',1,1003,1026,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1057,'F-9',1,1003,1027,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1058,'F-10',1,1003,1028,'Fallenzusatztext');
+INSERT INTO fallen 
+VALUES(1059,'F-11',1,1003,1029,'Zusatztext zu Falle F-11');
 commit;
 INSERT INTO aktionen 
 VALUES(1006,'A-1',1,'Brutstaette1',TO_DATE('2010-08-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'),1002,1004,NULL,'1. Aktionstext fuer Brutstaette 1');
