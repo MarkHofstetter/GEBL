811c1775bec27558d8b7f9b2249d30e6875b6809
Rudolf <oracle@.(none)>
2010-09-16 12:09:34 +0200

    Benutzerverwaltung modifiziert

--- gebl/public/js/admincudallpoints.js
+++ gebl/public/js/admincudallpoints.js
@@ -281,4 +281,11 @@
                             + "/zoom/" + map.getZoom();
     }
 
+    function listPersonen(){
+    window.location.href="/admin/listpersonen"
+                            + "/lat/" + map.getCenter().lat()
+                            + "/lon/" +  map.getCenter().lng()
+                            + "/zoom/" + map.getZoom();
+    }
+
 
